b6052b6212b92d4005614ad30208d9b8fe3dc191
Neamar <neamar@neamar.fr>
2014-02-17 11:18:41 +0100

    Requirements and imports

--- test/index.js
+++ test/index.js
@@ -1,1 +1,13 @@
- 
+'use strict';
+
+var restify = require('restify');
+var asyncJsonBodyParser = require('../lib/async_json_body_parser');
+
+describe("Async JSON body parser", function() {
+    var server = restify.createServer();
+    server.use(restify.bodyParser());
+
+    it("should handle form requests", function(done) {
+        done();
+    });
+});
