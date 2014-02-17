7111f66dd2bc32988a83ae08d83609d255bce64b
Neamar <neamar@neamar.fr>
2014-02-17 11:26:17 +0100

    Real test suite

--- test/index.js
+++ test/index.js
@@ -1,13 +1,49 @@
 'use strict';
 
+require("should");
+
 var restify = require('restify');
 var asyncJsonBodyParser = require('../lib/async_json_body_parser');
+
+var request = require('supertest');
+
+function expectPayload(payload) {
+    return function(res) {
+        res.body.should.eql(payload);
+    };
+}
 
 describe("Async JSON body parser", function() {
     var server = restify.createServer();
     server.use(restify.bodyParser());
+    server.post('/', function(req, res, next) {
+        res.send(req.params);
+        next();
+    });
 
-    it("should handle form requests", function(done) {
-        done();
+    it("should handle simple JSON requests", function(done) {
+        var payload = {};
+        request(server)
+            .post('/')
+            .send(payload)
+            .expect(200)
+            .expect(expectPayload(payload))
+            .end(done);
+    });
+
+    it("should handle long JSON requests", function(done) {
+        var payload = {};
+        var arbitraryContent = require('fs').readFileSync(__filename).toString();
+
+        for(var i = 0; i <= 100; i += 1) {
+            payload["a-key-" + i] = arbitraryContent;
+        }
+
+        request(server)
+            .post('/')
+            .send(payload)
+            .expect(200)
+            .expect(expectPayload(payload))
+            .end(done);
     });
 });
